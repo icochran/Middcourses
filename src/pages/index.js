ed6e450f0a6ad154e1ee5b6d15322f04bcac44d5
Agastya <agastya_5@hotmail.com>
2021-10-25 14:54:34 -0400

    added js files for each component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function MainPage() {
 
   return (
     <div className={styles.container}>

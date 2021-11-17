753b601f4d26f2959c2b5f3affd9e0df11b418d1
Issy Cochran <icochran@middlebury.edu>
2021-11-17 17:01:15 -0500

    Changed the webscraper so that the professors that are combined with the Fall and Spring format are added as seperate professors. I would test this alot to make sure I did not mess up there could be hidden problems

--- src/components/ProfDropDown.js
+++ src/components/ProfDropDown.js
@@ -3,8 +3,20 @@ import styles from "../styles/dropdown.module.css";
 import PropTypes from "prop-types";
 
 export default function ProfDropDown({profs, setProfName}) {
-  
-  const profList = profs.map(prof => (<li key={prof.prof_name}><a onClick={()=>setProfName(prof.prof_name)}>{prof.prof_name}</a></li>)) ;
+  const profSet = new Set();
+  const sortedProfs = []
+   
+  for (let i = 0; i < profs.length; i++) {
+    sortedProfs[i] = (profs[i].prof_name);
+  }
+  sortedProfs.sort();
+  sortedProfs.forEach((e) => {
+  if (!(profSet.has(e))) {
+    profSet.add(e)
+  }});
+  const professors = Array.from(profSet);
+
+  const profList = professors.map(prof => (<li key={prof}><a onClick={()=>setProfName(prof)}>{prof}</a></li>)) ;
   
   if(profs.length>1){
     return (

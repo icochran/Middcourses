f0d2f31cc88c8422e8bd7d1ecdfd6b4c7f0db62b
AbbyWright <afwright@middlebury.edu>
2021-11-19 12:46:18 -0500

    finished Filter tests for showing all categories

--- src/components/Filter.test.js
+++ src/components/Filter.test.js
@@ -6,40 +6,54 @@ import collection from "../../data/test-data.json";
 
 describe("Filter tests", () => {
     const handler = jest.fn();
-
     const deptSet = new Set();
     const sortedDepts = collection.map(course => course.dept).sort();
     sortedDepts.forEach(e => deptSet.add(e));
     const departments = Array.from(deptSet);
 
-    const profs = [];
+    const profSet = new Set();
+    const sortedProfs = collection.map((course) => {
+      for (let i = 0; i < course.profs.length; i++) {
+        const str = course.profs[i].prof_name;
+        if (str.includes("Fall 2021")) {
+          const ind = str.indexOf(":") + 2;
+          const lInd = str.indexOf(";");
+          return str.substring(ind,lInd);
+        }
+        if (str.includes("Spring 2022")) {
+          const lInd = str.indexOf(";");
+          const ind2 = str.indexOf(":", lInd) + 2;
+          return str.substring(ind2);
+        }
+        if (str !== "") {
+          return str;
+        }
+      }}).sort();
+    sortedProfs.forEach((e) => {
+      if (!(profSet.has(e))) {
+        profSet.add(e)
+      }});
+    const professors = Array.from(profSet);
 
     beforeEach(() => {
         handler.mockReset();
       });
 
-      //not sure this test is possible as it requires access to the card grid
-    test.skip("Clicking a department filters by that deparment", () => {
-        render(<Filter setFilterBy ={handler} departments = {departments} prof = {profs}/>);
-
-        const depbutton = screen.getByText("MATH");
-        
-
-        fireEvent.click(depbutton);
-        screen.debug();
-        expect(screen.queryByText(collection[1].class_name)).toBeVisible();
-        expect(screen.getByText(collection[0].class_name)).not.toBeVisible();
-    });
-
     test("Each department is shown as filter option", () => {
-        render(<Filter setFilterBy ={handler} departments = {departments} prof = {profs}/>);
+        render(<Filter setFilterBy ={handler} departments = {departments} prof = {professors}/>);
 
         departments.forEach((dept) => {
             expect(screen.getByText(dept)).toBeVisible();
           });
-
-
     })
+
+    test("Each professor is shown as filter option", () => {
+      render(<Filter setFilterBy ={handler} departments = {departments} prof = {professors}/>);
+
+      departments.forEach((prof) => {
+          expect(screen.getByText(prof)).toBeVisible();
+        });
+  })
 
 
 })

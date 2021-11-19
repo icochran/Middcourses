6b8c6149ba4f0095575980162afab8c269b37d3d
Agastya <agastya_5@hotmail.com>
2021-11-18 21:50:57 -0500

    started writing get/put methods

---
+++ src/lib/backend-utils.js
@@ -0,0 +1,26 @@
+import knexConfig from "../../knexfile";
+import knexInitializer from "knex";
+
+export const knex = knexInitializer(
+  knexConfig[process.env.NODE_ENV || "development"]
+);
+
+beforeEach(async () => {
+    await knex.migrate.rollback();
+    await knex.migrate.latest();
+    await knex.seed.run();
+  });
+
+export async function getCourses() {
+    const rows = await knex("Courses").select();
+    return rows;
+}
+
+
+export async function getCourse(id) {
+    const course = await knex("Courses").select().where("id", id);
+    if (course[0]===undefined){
+        return null
+    }
+    return course[0];
+}

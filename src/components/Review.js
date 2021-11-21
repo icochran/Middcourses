82bf53364a0246794af52b809d707dc784aadba4
Joseph Spada <jojospada@me.com>
2021-11-21 17:08:58 -0500

    more functionality for review

--- src/components/Review.js
+++ src/components/Review.js
@@ -20,6 +20,7 @@ export default function Review({ course, changeState}) {
     borderColor: `rgb(${0.5}, ${0.5}, ${0.5})`,
   };   
 
+  // should dry out this code eventually
   return (
     <div className={styles.classBox} style={classBoxStyle} >
       <div className={styles.classHeader}>
@@ -27,7 +28,7 @@ export default function Review({ course, changeState}) {
           <span>{courseName}</span>
         </div>
         <div>
-          <ProfDropDown profs={course.profs} setProf={setProf}/>
+          <ProfDropDown profs={course.profs} setProfName={setProf}/>
         </div>
       </div>
 
@@ -36,50 +37,54 @@ export default function Review({ course, changeState}) {
         <div className="difficulty">
             <p>Difficulty</p>
             <ButtonGroup aria-label="difficultyButtons">
-                <Button variant="secondary" onClick={() => setDifficulty(1)}>1</Button>
-                <Button variant="secondary" onClick={() => setDifficulty(2)}>2</Button>
-                <Button variant="secondary" onClick={() => setDifficulty(3)}>3</Button>
-                <Button variant="secondary" onClick={() => setDifficulty(4)}>4</Button>
-                <Button variant="secondary" onClick={() => setDifficulty(5)}>5</Button>
+                <Button variant={difficulty === 1 ? "primary" : "secondary"} onClick={() => setDifficulty(1)}>1</Button>
+                <Button variant={difficulty === 2 ? "primary" : "secondary"} onClick={() => setDifficulty(2)}>2</Button>
+                <Button variant={difficulty === 3 ? "primary" : "secondary"} onClick={() => setDifficulty(3)}>3</Button>
+                <Button variant={difficulty === 4 ? "primary" : "secondary"} onClick={() => setDifficulty(4)}>4</Button>
+                <Button variant={difficulty === 5 ? "primary" : "secondary"} onClick={() => setDifficulty(5)}>5</Button>
             </ButtonGroup>
         </div>
 
         <div className="interesting">
             <p>Interest</p>
             <ButtonGroup aria-label="interestingButtons">
-                <Button variant="secondary">1</Button>
-                <Button variant="secondary">2</Button>
-                <Button variant="secondary">3</Button>
-                <Button variant="secondary">4</Button>
-                <Button variant="secondary">5</Button>
+                <Button variant={interest === 1 ? "primary" : "secondary"} onClick={() => setInterest(1)}>1</Button>
+                <Button variant={interest === 2 ? "primary" : "secondary"} onClick={() => setInterest(2)}>2</Button>
+                <Button variant={interest === 3 ? "primary" : "secondary"} onClick={() => setInterest(3)}>3</Button>
+                <Button variant={interest === 4 ? "primary" : "secondary"} onClick={() => setInterest(4)}>4</Button>
+                <Button variant={interest === 5 ? "primary" : "secondary"} onClick={() => setInterest(5)}>5</Button>
             </ButtonGroup>
         </div>
 
         <div className="satisfaction">
             <p>Overall Satisfaction</p>
             <ButtonGroup aria-label="satisfactionButtons">
-                <Button variant="secondary">1</Button>
-                <Button variant="secondary">2</Button>
-                <Button variant="secondary">3</Button>
-                <Button variant="secondary">4</Button>
-                <Button variant="secondary">5</Button>
+                <Button variant={satisfaction === 1 ? "primary" : "secondary"} onClick={() => setSatisfaction(1)}>1</Button>
+                <Button variant={satisfaction === 2 ? "primary" : "secondary"} onClick={() => setSatisfaction(2)}>2</Button>
+                <Button variant={satisfaction === 3 ? "primary" : "secondary"} onClick={() => setSatisfaction(3)}>3</Button>
+                <Button variant={satisfaction === 4 ? "primary" : "secondary"} onClick={() => setSatisfaction(4)}>4</Button>
+                <Button variant={satisfaction === 5 ? "primary" : "secondary"} onClick={() => setSatisfaction(5)}>5</Button>
             </ButtonGroup>
         </div>
 
         <div className="time commitment">
             <p>Time Commitment</p>
             <ButtonGroup aria-label="timeButtons">
-                <Button variant="secondary">1</Button>
-                <Button variant="secondary">2</Button>
-                <Button variant="secondary">3</Button>
-                <Button variant="secondary">4</Button>
-                <Button variant="secondary">5</Button>
+                <Button variant={time === 1 ? "primary" : "secondary"} onClick={() => setTime(1)}>1</Button>
+                <Button variant={time === 2 ? "primary" : "secondary"} onClick={() => setTime(2)}>2</Button>
+                <Button variant={time === 3 ? "primary" : "secondary"} onClick={() => setTime(3)}>3</Button>
+                <Button variant={time === 4 ? "primary" : "secondary"} onClick={() => setTime(4)}>4</Button>
+                <Button variant={time === 5 ? "primary" : "secondary"} onClick={() => setTime(5)}>5</Button>
             </ButtonGroup>
         </div>
 
       </div>
       <button className="back" onClick= {changeState} >Back</button>
-      <button className="submit">Submit</button>
+      <button 
+        disabled={!difficulty || !interest || !satisfaction || !time} 
+        className="submit"
+        onClick={changeState}
+      >Submit</button>
     </div>
   );
 }

afd0143862eab9a0f1e47adc4f160782b93ef5f4
Joseph Spada <jojospada@me.com>
2021-11-22 12:48:14 -0500

    all but one lint error fixed

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -8,7 +8,6 @@ export default function CourseCard({ course, changeState, seeDetails }) {
   let backgroundColor;
   const [profName, setProfName] = useState(course.profs[0].prof_name);
   const reducer = (previousValue, currentValue) => previousValue + currentValue;
-
   // console.log("below is course");
   // console.log(course);
   
--- src/components/DetailedCourseCard.js
+++ src/components/DetailedCourseCard.js
@@ -1,14 +1,14 @@
 import styles from "../styles/Home.module.css";
 import PropTypes from "prop-types";
-import ProfDropDown from "./ProfDropDown";
-import { useState } from "react";
-import RatingBar from "./RatingBar.js";
+//import ProfDropDown from "./ProfDropDown";
+//import { useState } from "react";
+//import RatingBar from "./RatingBar.js";
 
 export default function DetailedCourseCard({ course, seeDetails }) {
   
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
+//const reducer = (previousValue, currentValue) => previousValue + currentValue;
 
-const courseDetails = {...course}
+//const courseDetails = {...course}
 
 //get the courseSatisfaction
 /*
@@ -36,7 +36,7 @@ const courseName = course.class_name;
 let courseDescription = course.course_desc;
 //to get the prereqs I should look for the last ) in the description. If it is more than say 10 characters away from the end set prereqs to undefined
 let prereqs; 
-if ((courseDescription.lastIndexOf(")"))!=-1 && courseDescription.length-courseDescription.lastIndexOf(")")<29) { //if there are prereqs
+if ((courseDescription.lastIndexOf(")"))!==-1 && courseDescription.length-courseDescription.lastIndexOf(")")<29) { //if there are prereqs
   //get the prereqs as a string
   const begginingOfPreReqs = courseDescription.lastIndexOf("(");
   const endOfPreReqs = courseDescription.lastIndexOf(")")+1;

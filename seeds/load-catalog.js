766076063909568544bfe3d440108989cdfee01c
Sam <Ssmadden@middlebury.edu>
2021-11-29 12:42:09 -0500

    added async

--- seeds/load-catalog.js
+++ seeds/load-catalog.js
@@ -1,7 +1,7 @@
 
 const fs = require("fs");
 
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   const contents = fs.readFileSync("./data/seed.json");
   const data = JSON.parse(contents);
 
@@ -52,7 +52,6 @@ exports.seed = function (knex) {
       }
     });
   });
-
 
   await knex("Courses")
     .del()

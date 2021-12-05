d9058c80926e9ca14df0268f0e6482e2728cbdba
Joseph Spada <jojospada@me.com>
2021-12-05 16:37:57 -0500

    fixed tests

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 //import { getAllCourses } from "../lib/backend-utils.js";
 import data from "../../data/seed.json"
-//import testData from "../../data/test-data.json"
+import testData from "../../data/test-data.json"
 
 export default function useCollection() {
     //const data = getAllCourses();
-    const [collection] = useState(data);
+    const [collection] = useState(testData);
 
     return collection;
 }

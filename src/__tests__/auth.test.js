d10e4a6526a84702c65727c90404dc714ccb4f4b
Issy Cochran <icochran@middlebury.edu>
2021-12-09 21:41:51 -0500

    Auth test done

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -15,6 +15,13 @@ describe("Client tests",()=>{
   });
   // test the other case in CardGrid.test.js
 
+  test("Secured login displays NavBar", async()=>{
+    useSession.mockReturnValue([{user: {name:"someone"}}, false]);
+    render(<SecureItem />);
+    fetchMock.flush(true);
+    expect(screen.getAllByTestId("navBar").length === 1).toBeTruthy();
+  });
+
   test("Insecure access is denied", async ()=>{
     useSession.mockReturnValue([undefined , false]);
     render(<SecureItem />);
